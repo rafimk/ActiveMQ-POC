22f25a0376504339762440642ec430fc502151e4
Muhammed Rafi <m.rafi@afad.gov.ae>
2022-12-16 10:16:08 +0400

    updaed with services

---
+++ src/EIS.Infrastructure/Scheduler/InboxOutboxPollerJobScheule.cs
@@ -0,0 +1,19 @@
+namespace EIS.Infrastructure.Scheduler
+
+public class InboxOutboxPollerJobSchedule : IJobSchedule
+{
+    private readonly IConfigurationManager _configManager;
+
+    public InboxOutboxPollerJobSchedule(IConfigurationManager configManager)
+    {
+        _configManager = configManager;
+        JobType = typeof(InboxOutboxPollerJob)
+
+    }
+
+    public Type JobType { get; }
+    public string GetCronExpression
+    {
+        return _configManager.GetBrokerConfiguration().InboxOutboxTimerPeriod;
+    }
+}
